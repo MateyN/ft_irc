ceb3483a0460a58b404b5e54ae4fd9cff30493a4
krioja <krioja@student.42lausanne.ch>
2023-08-09 12:39:30 +0200

    makefile and begin of main

---
+++ Makefile
@@ -0,0 +1,32 @@
+NAME			=	ircserv
+
+SRC_DIR			=	src/
+SRCS			=	main.cpp			\
+
+OBJ_DIR			= 	obj/
+OBJS			=	$(addprefix $(OBJ_DIR), $(SRCS:.cpp=.opp))
+
+CPP				=	c++
+CPPFLAGS		=	-Wall -Wextra -Werror
+STD98			=	-std=c++98
+RM				=	rm -rf
+
+all:				$(NAME)
+
+$(OBJ_DIR)%.opp :	$(SRC_DIR)%.cpp
+					@mkdir -p $(OBJ_DIR)
+					$(CPP) -c $(CPPFLAGS) $(STD98) -I inc/ $< -o $@
+
+$(NAME):			$(OBJS)	
+					$(CPP) $(CPPFLAGS) $(STD98) $(OBJS) -o $(NAME)
+
+clean:
+					$(RM) $(OBJ_DIR)
+					$(RM) $(OBJS)
+
+fclean:				clean
+					$(RM) $(NAME)
+
+re:					fclean all
+
+.PHONY:				all clean fclean re
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+static int	err(char *s)
+{
+	std::cerr << s << std::endl;
+	return (1);
+}
+
+int	main (int ac, char *av[])
+{
+	if (ac != 3) 
+		return (err("error: bad arguments, must have <port> <password>"));
+
+//	ConfigFile 	IRCconfig("./srcs/irc.config");
+//	Server		*serv = NULL;
+//	try
+//	{
+//		serv = new Server(av[1], av[2], &IRCconfig);
+//		serv->monitoring();
+//		serv->stop();
+//	}
+//	catch (std::exception &e)
+//	{
+//		std::cerr << "error: exception catched durng serv initialization: " << e.what() << std::endl;
+//		exit(EXIT_FAILURE);
+//	}
+//	delete serv;
+//	exit(EXIT_SUCCESS);
+}
