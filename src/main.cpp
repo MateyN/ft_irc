e8e324d5347595042fb6c14af56f9c82e6691eff
krioja <krioja@student.42lausanne.ch>
2023-08-15 11:26:31 +0200

    notes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <string>
 
-static int	err(char *s)
+static int	err(std::string s)
 {
 	std::cerr << s << std::endl;
 	return (1);
@@ -8,9 +9,9 @@ static int	err(char *s)
 
 int	main (int ac, char *av[])
 {
+	(void) av;
 	if (ac != 3) 
 		return (err("error: bad arguments, must have <port> <password>"));
-
 //	ConfigFile 	IRCconfig("./srcs/irc.config");
 //	Server		*serv = NULL;
 //	try
